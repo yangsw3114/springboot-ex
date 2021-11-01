ce5095cd62af50a008aaec4949736bb44bedfd16
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-11-01 13:09:27 +0900

    init

---
+++ src/main/java/com/douzone/springbootex/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.douzone.springbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * Bootstrap Class
+ *
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
